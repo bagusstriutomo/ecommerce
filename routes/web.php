99719b47dd5b987e1d3915f14292ce837f86ab3c
pamongsky <fahmimuhammad2405@gmail.com>
2025-10-13 02:25:04 +0700

    balikin lagi web.php

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,23 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProductController; // Pastikan ini ada (dari temanmu)
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-// Ini route halaman utama dari temanmu
-Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Ini route dashboard dari Breeze (milikmu)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Ini route profile dari Breeze (milikmu)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Ini route untuk login, register, dll. dari Breeze
+Route::get('/', [ProductController::class, 'index'])->name('home');
+
 require __DIR__.'/auth.php';
